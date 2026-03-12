4ee8a95ef5c936363270ff27cc01050ddd6874a3
ajithimpelox <ajith.karanam@impelox.com>
2026-03-12 12:29:49 +0530

    feat: initialize NestJS application with user management module
    - Created app.module.ts to set up the main application module with TypeORM and UsersModule.
    - Implemented main.ts to bootstrap the application and enable global validation pipes.
    - Added CreateUserDto and UpdateUserDto for user data validation.
    - Defined User entity with necessary fields and TypeORM decorators.
    - Developed UsersController to handle user-related HTTP requests.
    - Created UsersService for business logic related to user management.
    - Implemented e2e tests for the application.
    - Configured Jest for end-to-end testing.
    - Set up TypeScript configuration for building the project.

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: true })
+    name: string;
+
+    @Column({ unique: true, nullable: true })
+    email: string;
+
+    @Column({ nullable: true, select: false })
+    password: string;
+
+    @Column({ unique: true, nullable: true })
+    phone: string;
+
+    @Column({ nullable: true })
+    googleId: string;
+
+    @Column({ default: false })
+    isEmailVerified: boolean;
+
+    @Column({ default: false })
+    isPhoneVerified: boolean;
+
+    @Column({ default: 'USER' })
+    role: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
